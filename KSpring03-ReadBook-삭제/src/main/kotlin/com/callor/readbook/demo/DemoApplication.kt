78afb3d744416999880833934f4b37cc1836a9e4
soyeon-noh <1010qwerty12@gmail.com>
2021-10-27 11:51:43 +0900

    book project start

---
+++ KSpring03-ReadBook-삭제/src/main/kotlin/com/callor/readbook/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package com.callor.readbook.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
